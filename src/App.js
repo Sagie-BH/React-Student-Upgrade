273f8d65e5f8aaa238924acdbbeada286ee44fa7
Sagie Bar-Hame <2603sagie@gmail.com>
2020-12-04 13:04:18 +0200

    users with colored grades

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import StudentsGrid from './components/Student/StudentsGrid'
+import './bootstrapSolar.min.css';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://randomuser.me/api/?results=10`);
+      const users = result.data.results
+      setUsers(users);
+    }
+    fetchItems();
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StudentsGrid users={users} />
     </div>
   );
 }
---
+++ src/components/Student/Student.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Student = ({ user }) => {
+    let userName = user.name.title + '. ' + user.name.first + ' ' + user.name.last; 
+    let grade = Math.floor(Math.random() * 100);
+    let gradeStyle = {};
+    if (grade < 60){
+        gradeStyle = { color: 'red'}
+    }
+    else if (grade < 70 & grade > 60){
+        gradeStyle = { color: 'orange'}
+    }
+    else if (grade < 80 & grade > 70) {
+        gradeStyle = { color: 'purple'}
+    }
+    else if (grade < 90 & grade > 80) {
+        gradeStyle = { color: 'yellow'}
+    }
+    else{
+        gradeStyle = { color: 'green'}
+    }
+
+  return (
+    <div className="card m-2" style={{minWidth: '270px'}}>
+      <h5 className="card-title p2 mt-3 text-center">{userName}</h5>
+      <img
+        className="card-img-top"
+        src={user.picture.large}
+        alt=""
+      ></img>
+      <div className="card-body">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Gender: {user.gender}</li>
+          <li className="list-group-item">Cell: {user.cell}</li>
+          <li className="list-group-item">email: {user.email}</li>
+          <li style={gradeStyle} className='list-group-item'>Grade: {grade} </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Student;
